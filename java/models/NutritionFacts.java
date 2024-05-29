62cd602dd664cae3616d7ce926381190f441521a
MQAFSI23 <144584642+MQAFSI23@users.noreply.github.com>
2024-05-29 12:39:10 +0800

    Update NutritionFacts.java
    Remove unimportant code

--- java/models/NutritionFacts.java
+++ java/models/NutritionFacts.java
@@ -16,18 +16,6 @@ public class NutritionFacts extends Product {
         this.updater = updater;
     }
 
-    public void addNutrient(String nutrientName, double value) {
-        nutrients.put(nutrientName, value);
-    }
-
-    public void removeNutrient(String nutrientName) {
-        nutrients.remove(nutrientName);
-    }
-
-    public HashMap<String, Double> getNutrients() {
-        return nutrients;
-    }
-
     public double getNutrientValue(String nutrientName) {
         return nutrients.get(nutrientName);
     }
@@ -40,15 +28,7 @@ public class NutritionFacts extends Product {
         return updateDate;
     }
 
-    public void setUpdateDate(String updateDate) {
-        this.updateDate = updateDate;
-    }
-
     public String getUpdater() {
         return updater;
-    }
-
-    public void setUpdater(String updater) {
-        this.updater = updater;
     }
 }
